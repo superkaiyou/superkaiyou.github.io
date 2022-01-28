3acfc588ffdb563c64a90db5fda49ea1a02b52e5
kaiyou <lkyzxb@aliyun.com>
2022-01-28 21:50:49 +0800

    1.28

--- assets/js/sketchbang.js
+++ assets/js/sketchbang.js
@@ -6,7 +6,7 @@ let b;
 let mnote = ["A2","B2","C3","D3","E3","F3","G3","A3","B3","C4","D4","E4","F4","G4","A4","B4","C5","D5","E5","F5","G5","A5","B5"];
 let playlists = [],onplaylists = [];
 let playbutton,pausebutton,remixbutton,piano,pizzacato,marimba;
-var onplay;
+let onplay;
 let chord;
 let counter = 0;
 let can;
